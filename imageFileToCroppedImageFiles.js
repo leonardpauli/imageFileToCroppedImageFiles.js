bf1183bf55cc5825bbcf19275d104238a6236a0e
Leonard Pauli <leonardpauli@me.com>
2016-02-08 19:44:38 +0100

    Auto detects transparency and selects contain instead of cover as fill mode (if the image has transparency). This resolves #1.

--- imageFileToCroppedImageFiles.js
+++ imageFileToCroppedImageFiles.js
@@ -20,8 +20,34 @@ dataURItoBlob(dataURI) -> Blob
 
 function aspectContainImageCrop(image, size, isPng) {
 
+  // Only possibly necessary for remote images to prevent
+  // x-origin error (lookup if it's needed at all)
+  image.crossOrigin='anonymous'
+
+  // Get a canvas to draw on
+  var canvas = document.createElement('canvas');
+  var ctx = canvas.getContext('2d');
+
+  // Figure out if the image has transparancy
+  isPng = isPng | (function (ctx, canvas, image) {
+    canvas.setAttribute('width', image.width/3);
+    canvas.setAttribute('height', image.height/3);
+
+    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+    var imgData = ctx.getImageData(0,0,canvas.width,canvas.height).data;
+    var isTrans = false;
+    for (var i = 3; i<imgData.length; i+=4)
+      if (imgData[i]<255) {
+        isTrans = true;
+        break;
+      }
+
+    return isTrans;
+  })(ctx, canvas, image);
+
   // Get smallest source/destination size factor
-  var scale = Math.min(image.width/size.w, image.height/size.h);
+  var scale = (isPng?Math.min:Math.max)(image.width/size.w, image.height/size.h);
 
   // Scale destination so that smallest
   // side matches smallest source side
@@ -41,23 +67,21 @@ function aspectContainImageCrop(image, size, isPng) {
   };
   console.log(imageScale, toSize);
   
-  // Get a canvas to draw on
-  var canvas = $('<canvas/>');
-  canvas.attr({width:toSize.w*imageScale,height:toSize.h*imageScale});
-  canvas.hide().appendTo('body');
-
-  var ctx = canvas.get(0).getContext('2d');
+  // Reset the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  canvas.setAttribute('width', toSize.w*imageScale);
+  canvas.setAttribute('height', toSize.h*imageScale);
   
   // Cut and resize image by drawing it scaled in a frame
   ctx.drawImage(image, cutStart.x, cutStart.y, toSize.w, toSize.h, 0, 0, toSize.w*imageScale, toSize.h*imageScale);
   
   // Convert image to data
   var base64ImageData;
-  if (isPng) base64ImageData = canvas.get(0).toDataURL("image/png");
-  else base64ImageData = canvas.get(0).toDataURL("image/jpeg", 0.8);
+  if (isPng) base64ImageData = canvas.toDataURL("image/png");
+  else base64ImageData = canvas.toDataURL("image/jpeg", 0.8);
 
   // Clean up
-  canvas.remove();
+  //canvas.parent().removeChild(canvas);
 
   return base64ImageData;
 }
